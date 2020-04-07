e7139665d0bd4684a0a33139d7ff3fcbef4dce52
Arita Malik <hello@coderport.com>
2020-04-07 17:05:05 +0530

    Connected with Eureka Naming Server

--- src/main/java/com/codelovin/microservices/operandsservice/OperandsServiceApplication.java
+++ src/main/java/com/codelovin/microservices/operandsservice/OperandsServiceApplication.java
@@ -2,8 +2,10 @@ package com.codelovin.microservices.operandsservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class OperandsServiceApplication {
 
 	public static void main(String[] args) {
